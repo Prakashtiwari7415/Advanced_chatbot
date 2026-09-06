e3df7d49e3734247f657a5ceb4b570d237d59ad3
Prakashtiwari7415 <prakashtiwari812021@gmail.com>
2026-09-06 18:41:12 +0530

    Made some changes

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -1,14 +1,17 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = ChatOpenAI()
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.7
+)
 
 class ChatState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
