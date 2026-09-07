03c902179980fa24cb527b666a349686bf8ecfc8
Prakashtiwari7415 <prakashtiwari812021@gmail.com>
2026-09-07 19:43:10 +0530

    added database in chatbot

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -2,7 +2,7 @@ from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver #INmomery saver is replaced with sqlite saver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
 
@@ -21,8 +21,14 @@ def chat_node(state: ChatState):
     response = llm.invoke(messages)
     return {"messages": [response]}
 
+
+#we are connecting our database with sqlite
+connt=sqlite3.connect(databse='chatbot.db',check_same_thread=False) #Here false means we are saying that we will use this database for dufferent different thread
+
+
+
 # Checkpointer
-checkpointer = InMemorySaver()
+checkpointer = SqliteSaver(conn=connt)
 
 graph = StateGraph(ChatState)
 graph.add_node("chat_node", chat_node)
@@ -30,3 +36,12 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+## we are making function to connect our sqlite database with our frontend
+def retrieve_all_threads():
+    all_threads=set()
+    for checkpointer in checkpointer.list(None):
+        all_threads.add(checkpointer.config['configurable']['thread_id'])
+
+    return list(all_threads)
+     
